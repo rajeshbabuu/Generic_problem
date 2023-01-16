bc9d0e2187fe28b4a4ac21ae054552a121922938
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-16 21:09:51 +0530

    Find the Max Integer

---
+++ Generics/Maxnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Maxnum
+    {
+        public static int MaxIntegerNumber(int val1, int val2, int val3)
+        {
+            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0)
+                return val1;
+            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0)
+                return val2;
+            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0)
+                return val3;
+            return val1;
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Based Problems");
+
+            int output = Maxnum.MaxIntegerNumber(11, 111, 1111);
+            Console.WriteLine("\nMax integer number is: " + output);
+        }
+    }
+}
