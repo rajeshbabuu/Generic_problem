4e5a5b47e03eb2aa08d96175b3ace8171d5f9724
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-16 21:30:16 +0530

    Max number at 3rd Position

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -9,7 +9,7 @@ namespace Generics
             Console.WriteLine("Welcome to Generic Based Problems");
 
             int output = Maxnum.MaxIntegerNumber(11, 1111, 111);
-            Console.WriteLine("\nMax integer number is: " + output);
+            Console.WriteLine("\nMax  integer number is: " + output);
         }
     }
 }
