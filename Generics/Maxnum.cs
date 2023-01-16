ade8fca09bad67dabeaec14e959a18ecf758a407
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-16 21:18:22 +0530

    Using for Parameters

--- Generics/Maxnum.cs
+++ Generics/Maxnum.cs
@@ -8,15 +8,36 @@ namespace Generics
 {
     public class Maxnum
     {
-        public static string MaxIntegerNumber(string val1, string val2, string val3)
+        public class MaxNumberCheck<T> where T : IComparable
         {
-            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0)
+            public T val1, val2, val3, val4;
+
+            public MaxNumberCheck(T val1, T val2, T val3, T val4)
+            {
+                this.val1 = val1;
+                this.val2 = val2;
+                this.val3 = val3;
+                this.val4 = val4;
+            }
+
+            public static T MaxNumber(T val1, T val2, T val3, T val4)
+            {
+                if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0 && val1.CompareTo(val4) > 0)
+                    return val1;
+                if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0 && val2.CompareTo(val4) > 0)
+                    return val2;
+                if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0 && val3.CompareTo(val4) > 0)
+                    return val3;
+                if (val4.CompareTo(val1) > 0 && val4.CompareTo(val2) > 0 && val4.CompareTo(val3) > 0)
+                    return val4;
                 return val1;
-            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0)
-                return val2;
-            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0)
-                return val3;
-            return val1;
+            }
+
+            public T MaxMethod()
+            {
+                T max = MaxNumberCheck<T>.MaxNumber(this.val1, this.val2, this.val3, this.val4);
+                return max;
+            }
         }
     }
 }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Generics
+﻿using static Generics.Maxnum;
+
+namespace Generics
 {
     internal class Program
     {
@@ -6,8 +8,14 @@
         {
             Console.WriteLine("Welcome to Generic Based Problems");
 
-            string output = Maxnum.MaxIntegerNumber("Apple", "Peach", "Banana");
-            Console.WriteLine("\nMax string word is: " + output);
+            MaxNumberCheck<int> objint = new MaxNumberCheck<int>(11, 111, 1111, 11111);
+            Console.WriteLine("\nMax integer number is: " + objint.MaxMethod());
+
+            MaxNumberCheck<float> objfloat = new MaxNumberCheck<float>(1, 11, 111, 1111);
+            Console.WriteLine("\nMax float number is: " + objfloat.MaxMethod());
+
+            MaxNumberCheck<string> objstring = new MaxNumberCheck<string>("Apple", "Peach", "Banana", "Orange");
+            Console.WriteLine("\nMax string word is: " + objstring.MaxMethod());
         }
     }
 }
