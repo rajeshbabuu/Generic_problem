750d776e32dfc6b20e2b69bf92255cf3baf0b27e
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-16 22:50:12 +0530

    Refactor using Generics Method

--- Generics/Maxnum.cs
+++ Generics/Maxnum.cs
@@ -8,7 +8,7 @@ namespace Generics
 {
     public class Maxnum
     {
-        public static float MaxIntegerNumber(float val1, float val2, float val3)
+        public T MaxNumber<T>(T val1, T val2, T val3) where T : IComparable
         {
             if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0)
                 return val1;
@@ -16,7 +16,7 @@ namespace Generics
                 return val2;
             if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0)
                 return val3;
-            return val2;
+            return val1;
         }
         
     }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -8,8 +8,17 @@ namespace Generics
         {
             Console.WriteLine("Welcome to Generic Based Problems");
 
-            float output = Maxnum.MaxIntegerNumber(111, 1, 12);
-            Console.WriteLine("\nMax   float number is: " + output);
+
+            Maxnum objMaxNumber = new Maxnum();
+
+            int output1 = objMaxNumber.MaxNumber<int>(11, 111, 1111);
+            Console.WriteLine("\nMax integer number is: " + output1);
+
+            float output2 = objMaxNumber.MaxNumber<float>(1, 11, 111);
+            Console.WriteLine("\nMax float number is: " + output2);
+
+            string output3 = objMaxNumber.MaxNumber<string>("Apple", "Peach", "Banana");
+            Console.WriteLine("\nMax string word is: " + output3);
         }
     }
 }
