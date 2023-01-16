0166fac78422528edd04015af1499a6e1a5da80e
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-16 22:46:22 +0530

    Max Float at 2nd Position

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -9,7 +9,7 @@ namespace Generics
             Console.WriteLine("Welcome to Generic Based Problems");
 
             float output = Maxnum.MaxIntegerNumber(111, 1, 12);
-            Console.WriteLine("\nMax  float number is: " + output);
+            Console.WriteLine("\nMax   float number is: " + output);
         }
     }
 }
