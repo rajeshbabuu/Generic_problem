a322f4e5f8f68a721f9b7acc10c21eae8a54e309
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-16 21:33:53 +0530

    Max Float at 1st Position

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -8,8 +8,8 @@ namespace Generics
         {
             Console.WriteLine("Welcome to Generic Based Problems");
 
-            int output = Maxnum.MaxIntegerNumber(11, 1111, 111);
-            Console.WriteLine("\nMax  integer number is: " + output);
+            float output = Maxnum.MaxIntegerNumber(111, 1, 1251);
+            Console.WriteLine("\nMax float number is: " + output);
         }
     }
 }
