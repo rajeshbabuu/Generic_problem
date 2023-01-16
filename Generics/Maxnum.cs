039b44aeb1311021193e4180f910640090144f37
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-16 21:24:34 +0530

    Max number at 1st Position

--- Generics/Maxnum.cs
+++ Generics/Maxnum.cs
@@ -8,67 +8,16 @@ namespace Generics
 {
     public class Maxnum
     {
-        public class MaxNumberCheck<T> where T : IComparable
+        public static int MaxIntegerNumber(int val1, int val2, int val3)
         {
-            public T val1, val2, val3, val4;
-
-            public MaxNumberCheck(T val1, T val2, T val3, T val4)
-            {
-                this.val1 = val1;
-                this.val2 = val2;
-                this.val3 = val3;
-                this.val4 = val4;
-            }
-
-            public static T MaxNumber(T val1, T val2, T val3, T val4)
-            {
-                if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0 && val1.CompareTo(val4) > 0)
-                    return val1;
-                if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0 && val2.CompareTo(val4) > 0)
-                    return val2;
-                if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0 && val3.CompareTo(val4) > 0)
-                    return val3;
-                if (val4.CompareTo(val1) > 0 && val4.CompareTo(val2) > 0 && val4.CompareTo(val3) > 0)
-                    return val4;
+            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0)
                 return val1;
-            }
-
-            public T MaxMethod()
-            {
-                T max = MaxNumberCheck<T>.MaxNumber(this.val1, this.val2, this.val3, this.val4);
-                return max;
-            }
-
-            // For finding the Max Value
-            public T[] value;
-            public MaxNumberCheck(T[] value)
-            {
-                this.value = value;
-            }
-
-            public T[] Sort(T[] values)
-            {
-                Array.Sort(values);
-                return values;
-            }
-
-            public T MaxValue(params T[] values)
-            {
-                var sorted_values = Sort(values);
-                return sorted_values[^1];
-            }
-
-            public T MaxMethod2()
-            {
-                var max = MaxValue(this.value);
-                return max;
-            }
-
-            public void PrintMaxValue()
-            {
-                var max = MaxValue(this.value);
-                Console.WriteLine("\nMax Value is: " + max);
-            }
+            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0)
+                return val2;
+            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0)
+                return val3;
+            return val1;
         }
+        
     }
 }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -8,18 +8,8 @@ namespace Generics
         {
             Console.WriteLine("Welcome to Generic Based Problems");
 
-            MaxNumberCheck<int> objint = new MaxNumberCheck<int>(11, 111, 1111, 11111);
-            Console.WriteLine("\nMax integer number is: " + objint.MaxMethod());
-
-            MaxNumberCheck<float> objfloat = new MaxNumberCheck<float>(1, 11, 111, 1111);
-            Console.WriteLine("\nMax float number is: " + objfloat.MaxMethod());
-
-            MaxNumberCheck<string> objstring = new MaxNumberCheck<string>("Apple", "Peach", "Banana", "Orange");
-            Console.WriteLine("\nMax string word is: " + objstring.MaxMethod());
-
-            int[] arr = { 11, 111, 1111, 11111, 111111 };
-            MaxNumberCheck<int> objvalue = new MaxNumberCheck<int>(arr);
-            objvalue.PrintMaxValue();
+            int output = Maxnum.MaxIntegerNumber(1111, 111, 11);
+            Console.WriteLine("\nMax integer number is: " + output);
         }
     }
 }
